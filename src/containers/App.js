506a27432ef3b83651b4402924494599880f82db
Edward Roberson <61763082+greedy17@users.noreply.github.com>
2020-06-11 04:22:47 -0400

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import Cardlist from '../components/Cardlist';
+import Searchbox from '../components/Searchbox';
+import './App.css';
+import Scroll from "../components/Scroll";
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()
+    .then(users => this.setState({robots: users})))
+  }
+
+  onSearchChange = (event) => {
+    this.setState({searchfield: event.target.value})
+    }
+  
+  render() {
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())})
+      if (this.state.robots.length === 0) {
+        return <h1>Loading</h1>
+      }else {
+        return (
+        <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <Searchbox searchChange={this.onSearchChange}/>
+        <Scroll>
+        <Cardlist robots={filteredRobots}/>
+        </Scroll>
+        </div>
+        
+      );
+    }  
+  }
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+   id: 1,
+   name: 'Pocantar Reedis',
+   username: 'Poca',
+   email: 'princess@april.cool'
+  },
+  {
+    id: 2,
+    name: 'Avalanche Rox',
+    username: 'Ava',
+    email: 'angel@june.cool'
+   },
+   {
+    id: 3,
+    name: 'James Jake',
+    username: 'Jamo',
+    email: 'him@may.cool'
+   },
+   {
+    id: 4,
+    name: 'Eleanor Westin',
+    username: 'Ellie',
+    email: 'swellells@july.cool'
+   },
+   {
+    id: 5,
+    name: 'Bongo Manny',
+    username: 'Beam',
+    email: 'Beamed@jan.coole'
+   },
+   {
+    id: 6,
+    name: 'Ominous Preston',
+    username: 'Minnie',
+    email: 'Minnieappleyes@dec.cool'
+   },
+   {
+    id: 7,
+    name: 'Skeebo Prime',
+    username: 'Preme',
+    email: 'skp@april.cool'
+   },
+   {
+    id: 8,
+    name: 'Brody',
+    username: 'Brody',
+    email: 'Brody@Jan.cool'
+   },
+   {
+    id: 9,
+    name: 'Model Nine',
+    username: 'Reelrobo',
+    email: 'abotslife@feb.cool'
+   },
+   {
+    id: 10,
+    name: 'Joseph Wimberley',
+    username: 'Jojo',
+    email: 'Jo3@dec.cool'
+   }
+]
